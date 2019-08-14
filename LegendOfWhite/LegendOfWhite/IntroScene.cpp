0a313b97e634032fbacb860f968771b6b8231e95
abbd2 <48604519+abbd2@users.noreply.github.com>
2019-08-14 18:00:48 +0900

    0814work

--- LegendOfWhite/LegendOfWhite/IntroScene.cpp
+++ LegendOfWhite/LegendOfWhite/IntroScene.cpp
@@ -16,24 +16,29 @@ void IntroScene::Update(float Delta)
 
 }
 
-void IntroScene::Render(Gdiplus::Graphics* MemG)
-{		
-	Gdiplus::Rect rect(0, 0, WIDTH, HEIGHT);
+
+void IntroScene::Render(Gdiplus::Graphics * MemG)
+{
+	Gdiplus::Rect introImgrc(0, 0, WIDTH, HEIGHT);
+	Gdiplus::Rect newplayImgrc(WIDTH / 2 - BTN_WIDTH / 2, HEIGHT / 2, BTN_WIDTH, BTN_HEIGHT);
+	Gdiplus::Rect loadplayImgrc(WIDTH / 2 - BTN_WIDTH / 2, HEIGHT / 2 + BTN_HEIGHT + 14, BTN_WIDTH, BTN_HEIGHT);
+	Gdiplus::Rect gameendImgrc(WIDTH / 2 - BTN_WIDTH / 2, HEIGHT / 2 + BTN_HEIGHT * 2 + 28, BTN_WIDTH, BTN_HEIGHT);
 
 	Gdiplus::Bitmap bm(WIDTH, HEIGHT, PixelFormat32bppARGB);
 	Gdiplus::Graphics temp(&bm);
 
-	Gdiplus::SolidBrush WhiteBrush(Gdiplus::Color(255, 255, 255, 255));
-	MemG->FillRectangle(&WhiteBrush, rect);
-
 	introImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\introImg.png"));
+	newplayImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\newplay.png"));
+	loadplayImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\loadplay.png"));
+	gameendImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\gameend.png"));
 
-
-	temp.DrawImage(introImg.lock().get(),
-		rect, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(introImg.lock().get(), introImgrc, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(newplayImg.lock().get(), newplayImgrc, 0, 0, 368, 134, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(loadplayImg.lock().get(), loadplayImgrc, 0, 0, 368, 134, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(gameendImg.lock().get(), gameendImgrc, 0, 0, 368, 134, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
 
 	//그려줄 screen좌표의 rect
-	Gdiplus::Rect screenPosRect(0,0, WIDTH, HEIGHT);
+	Gdiplus::Rect screenPosRect(0, 0, WIDTH, HEIGHT);
 
 	MemG->DrawImage(&bm, screenPosRect);
 }
--- LegendOfWhite/LegendOfWhite/IntroScene.h
+++ LegendOfWhite/LegendOfWhite/IntroScene.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Scene.h"
 
+#define BTN_WIDTH 270
+#define BTN_HEIGHT 88
+
 class IntroScene : public Scene
 {
 public:
@@ -11,5 +14,8 @@ public:
 	void Render(Gdiplus::Graphics* MemG);
 	void SendLButtonDown(UINT nFlags, CPoint point);
 	std::weak_ptr<Gdiplus::Image> introImg;
+	std::weak_ptr<Gdiplus::Image> newplayImg;
+	std::weak_ptr<Gdiplus::Image> loadplayImg;
+	std::weak_ptr<Gdiplus::Image> gameendImg;
 };
 

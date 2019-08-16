b88609c5a70c76c0635dda50800deffbc502e9ae
abbd2 <48604519+abbd2@users.noreply.github.com>
2019-08-16 15:54:15 +0900

    ff

--- LegendOfWhite/LegendOfWhite/StatusScene.cpp
+++ LegendOfWhite/LegendOfWhite/StatusScene.cpp
@@ -33,6 +33,113 @@ void StatusScene::Render(Gdiplus::Graphics* MemG)
 	//그려줄 screen좌표의 rect
 	Gdiplus::Rect screenPosRect(0, 0, WIDTH, HEIGHT);
 
+	// 스탯 좌표
+	Gdiplus::Rect statrc(STATX_WIDTH, STATY_HEIGHT, STAT_WIDTH, STAT_HEIGHT);
+	Gdiplus::Rect statrc2(STATX_WIDTH, STATY_HEIGHT + STAT_HEIGHT + STAT_HITV, STAT_WIDTH, STAT_HEIGHT);
+	Gdiplus::Rect statrc3(STATX_WIDTH, STATY_HEIGHT + STAT_HEIGHT * 2 + STAT_HITV * 2, STAT_WIDTH, STAT_HEIGHT);
+	Gdiplus::Rect statrc4(STATX_WIDTH, STATY_HEIGHT + STAT_HEIGHT * 3 + STAT_HITV * 3, STAT_WIDTH, STAT_HEIGHT);
+	Gdiplus::Rect statrc5(STATX_WIDTH, STATY_HEIGHT + STAT_HEIGHT * 4 + STAT_HITV * 4, STAT_WIDTH, STAT_HEIGHT);
+
+	// 스탯 퍼센트
+	Gdiplus::Rect statPrc(STATPX_WIDTH, STATPY_HEIGHT, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statPrc2(STATPX_WIDTH, STATPY_HEIGHT + STATP_HEIGHT + STATP_HITV, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statPrc3(STATPX_WIDTH, STATPY_HEIGHT + STATP_HEIGHT * 2 + STATP_HITV * 2, STATP_WIDTH, STATP_HEIGHT);
+
+	// 스탯 업그레이드
+	Gdiplus::Rect statUrc(STATUX_WIDTH, STATUY_HEIGHT, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUrc2(STATUX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT + STATP_HITV, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUrc3(STATUX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT * 2 + STATP_HITV * 2, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUrc4(STATUX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT * 3 + STATP_HITV * 3, STATP_WIDTH, STATP_HEIGHT);
+
+	// 스탯 업그레이드 퍼센트
+	Gdiplus::Rect statUPrc(STATUPX_WIDTH, STATUY_HEIGHT, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUPrc2(STATUPX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT + STATP_HITV, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUPrc3(STATUPX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT * 2 + STATP_HITV * 2, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUPrc4(STATUPX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT * 3 + STATP_HITV * 3, STATP_WIDTH, STATP_HEIGHT);
+	Gdiplus::Rect statUPrc5(STATUPX_WIDTH, STATUY_HEIGHT + STATP_HEIGHT * 4 + STATP_HITV * 4, STATP_WIDTH, STATP_HEIGHT);
+	
+	// 왼쪽 작은 스탯 버튼
+	Gdiplus::Rect lminiStatrc(LMINISTATX_WIDTH, LMINISTATY_HEIGHT, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect lminiStatrc2(LMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT + MINISTAT_HITV, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect lminiStatrc3(LMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT * 2 + MINISTAT_HITV * 2, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect lminiStatrc4(LMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT * 3 + MINISTAT_HITV * 3, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+
+	// 오른쪽 작은 스탯 버튼
+	Gdiplus::Rect rminiStatrc(RMINISTATX_WIDTH, LMINISTATY_HEIGHT, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect rminiStatrc2(RMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT + MINISTAT_HITV, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect rminiStatrc3(RMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT * 2 + MINISTAT_HITV * 2, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect rminiStatrc4(RMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT * 3 + MINISTAT_HITV * 3, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+	Gdiplus::Rect rminiStatrc5(RMINISTATX_WIDTH, LMINISTATY_HEIGHT + LMINISTAT_HEIGHT * 4 + MINISTAT_HITV * 4, LMINISTAT_WIDTH, LMINISTAT_HEIGHT);
+
+
+
+	// 스탯 죄표
+	bgImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\bgImg.png"));
+	temp.DrawImage(bgImg.lock().get(),
+		statrc, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	//bgImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\bgImg.png"));
+	temp.DrawImage(bgImg.lock().get(),
+		statrc2, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statrc3, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statrc4, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statrc5, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+
+	// 스탯 퍼센트
+	temp.DrawImage(bgImg.lock().get(),
+		statPrc, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statPrc2, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statPrc3, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+
+	// 스탯 업그레이드
+	temp.DrawImage(bgImg.lock().get(),
+		statUrc, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUrc2, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUrc3, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUrc4, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+
+	// 스탯 업그레이드 퍼센트
+	temp.DrawImage(bgImg.lock().get(),
+		statUPrc, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUPrc2, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUPrc3, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUPrc4, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		statUPrc5, 0, 0, WIDTH, HEIGHT, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+
+	// 왼쪽 작은 스탯 버튼
+	bgImg = AssetManager::GetInstance()->GetImage(TEXT("Asset\\wall_blue.png"));
+	temp.DrawImage(bgImg.lock().get(),
+		lminiStatrc, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		lminiStatrc2, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		lminiStatrc3, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		lminiStatrc4, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+
+	// 오른쪽 작은 스탯 버튼
+	temp.DrawImage(bgImg.lock().get(),
+		rminiStatrc, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		rminiStatrc2, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		rminiStatrc3, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		rminiStatrc4, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+	temp.DrawImage(bgImg.lock().get(),
+		rminiStatrc5, 0, 0, 128, 128, Gdiplus::Unit::UnitPixel, nullptr, 0, nullptr);
+
 	MemG->DrawImage(&bm, screenPosRect);
 
 	//for (auto& it : objectVec)
--- LegendOfWhite/LegendOfWhite/StatusScene.h
+++ LegendOfWhite/LegendOfWhite/StatusScene.h
@@ -1,6 +1,40 @@
 #pragma once
 #include "Scene.h"
 
+// 스탯
+#define STATX_WIDTH 560 // 위치값
+#define STATY_HEIGHT 155 // 위치값
+#define STAT_WIDTH 100 // 길이값
+#define STAT_HEIGHT 53 // 높이값
+
+// 퍼센트 스탯
+#define STATPX_WIDTH 680 // 위치값
+#define STATPY_HEIGHT 160 // 위치값
+#define STATP_WIDTH 113 // 길이값
+#define STATP_HEIGHT 42 // 길이값
+
+// 사이 간격
+#define STAT_HITV 39 // 스탯 위아래 사이 간격
+#define STATP_HITV 50 // 스탯 퍼센트 위아래 사이 간격
+#define MINISTAT_HITV 65 // 왼쪽 작은 스탯 버튼 위아래 사이 간격
+
+// 스탯 업그레이드
+#define STATUX_WIDTH 1002 // 위치값
+#define STATUY_HEIGHT 160 // 위치값
+
+// 스탯 업그레이드 퍼센트
+#define STATUPX_WIDTH 1131 // 위치값
+
+// 왼쪽 작은 스탯 버튼
+#define LMINISTATX_WIDTH 1058 // 위치값
+#define LMINISTATY_HEIGHT 192 // 위치값
+#define LMINISTAT_WIDTH 66 // 길이값
+#define LMINISTAT_HEIGHT 28 // 길이값
+
+// 오른쪽 작은 스탯 버튼
+#define RMINISTATX_WIDTH 1187 // 위치값
+
+
 class StatusScene : public Scene
 {
 public:
@@ -14,5 +48,6 @@ public:
 
 public:
 	std::weak_ptr<Gdiplus::Image> statImg;
+	std::weak_ptr<Gdiplus::Image> bgImg;
 
 };
